e482a8c09c75e7fa3c1f442d6603b1e5f2f60287
yoktave-yoknel <32586011+yoktave-yoknel@users.noreply.github.com>
2020-09-26 21:04:45 +0900

    最初のテストをpass
    フィボナッチ数列の0番目を確認するテストをpass
    ※テストクラスの中に実装コードがあるのは本の記載のまま。
    　(実装が関数1つだけであるため)

---
+++ fibonacci/src/test/java/fibonacci/FibonacciTest.java
@@ -0,0 +1,14 @@
+package fibonacci;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FibonacciTest {
+    int fib(int n) {
+        return 0;
+    }
+    @Test
+    public void testFibonacci() {
+        assertEquals(0, fib(0));
+    }
+}
