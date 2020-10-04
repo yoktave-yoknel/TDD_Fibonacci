4ad2d3f3e4676c5acabace6a5393ec09468c2267
yoktave-yoknel <32586011+yoktave-yoknel@users.noreply.github.com>
2020-10-04 19:05:26 +0900

    テストをリファクタリング
    入力値を期待値の組でテストを回すようにする

--- fibonacci/src/test/java/fibonacci/FibonacciTest.java
+++ fibonacci/src/test/java/fibonacci/FibonacciTest.java
@@ -12,7 +12,8 @@ public class FibonacciTest {
 
     @Test
     public void testFibonacci() {
-        assertEquals(0, fib(0));
-        assertEquals(1, fib(1));
+        int cases[][] = { { 0, 0 }, { 1, 1 } };
+        for (int i = 0; i < cases.length; i++)
+            assertEquals(cases[i][1], fib(cases[i][0]));
     }
 }
