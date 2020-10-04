05db945b2e6097a2e15c9b59d1d0847819dfbd57
yoktave-yoknel <32586011+yoktave-yoknel@users.noreply.github.com>
2020-10-04 19:10:56 +0900

    fib(3)をグリーン化
    とりあえずテストOKにする。
    リファクタリングは次でやる。

--- fibonacci/src/test/java/fibonacci/FibonacciTest.java
+++ fibonacci/src/test/java/fibonacci/FibonacciTest.java
@@ -7,7 +7,9 @@ public class FibonacciTest {
     int fib(int n) {
         if (n == 0)
             return 0;
-        return 1;
+        if (n <= 2)
+            return 1;
+        return 2;
     }
 
     @Test
