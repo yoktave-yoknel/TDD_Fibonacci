4ce71a455f631d6e81b1e7c1dfb7edba835b26df
yoktave-yoknel <32586011+yoktave-yoknel@users.noreply.github.com>
2020-10-04 19:21:01 +0900

    リファクタリングその3
    さらに一般化を進める。
    if(n<=2)の条件をif(n==1)に緩和しても動作は同じ。

--- fibonacci/src/test/java/fibonacci/FibonacciTest.java
+++ fibonacci/src/test/java/fibonacci/FibonacciTest.java
@@ -7,7 +7,7 @@ public class FibonacciTest {
     int fib(int n) {
         if (n == 0)
             return 0;
-        if (n <= 2)
+        if (n == 1)
             return 1;
         return fib(n - 1) + fib(n - 2);
     }
