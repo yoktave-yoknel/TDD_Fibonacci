03edba87e9839841120fedc28642a9e125b04c1d
yoktave-yoknel <32586011+yoktave-yoknel@users.noreply.github.com>
2020-10-04 19:08:04 +0900

    fib(3)のテストを追加
    テストを追加することでレッドの状態になった。

--- fibonacci/src/test/java/fibonacci/FibonacciTest.java
+++ fibonacci/src/test/java/fibonacci/FibonacciTest.java
@@ -12,7 +12,7 @@ public class FibonacciTest {
 
     @Test
     public void testFibonacci() {
-        int cases[][] = { { 0, 0 }, { 1, 1 }, { 2, 1 } };
+        int cases[][] = { { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 } };
         for (int i = 0; i < cases.length; i++)
             assertEquals(cases[i][1], fib(cases[i][0]));
     }
